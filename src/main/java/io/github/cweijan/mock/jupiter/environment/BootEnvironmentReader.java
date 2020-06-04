48e5f1aa512281d6309abcef2c4249e838ae00b3
cweijan <cweijan@163.com>
2020-06-04 14:56:48 +0800

    support bootstrap config

--- src/main/java/io/github/cweijan/mock/jupiter/environment/BootEnvironmentReader.java
+++ src/main/java/io/github/cweijan/mock/jupiter/environment/BootEnvironmentReader.java
@@ -3,7 +3,11 @@ package io.github.cweijan.mock.jupiter.environment;
 import org.springframework.boot.context.config.ConfigFileApplicationListener;
 import org.springframework.boot.context.properties.source.ConfigurationPropertySources;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.StandardEnvironment;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author cweijan
@@ -11,12 +15,25 @@ import org.springframework.core.env.StandardEnvironment;
  */
 public class BootEnvironmentReader extends ConfigFileApplicationListener implements HttpMockContextReader {
 
+    private static final String BOOTSTRAP_PROPERTY_SOURCE_NAME = "bootstrap";
+
     public ConfigurableEnvironment environment;
+
 
     public BootEnvironmentReader() {
         this.environment = new StandardEnvironment();
         ConfigurationPropertySources.attach(environment);
         this.addPropertySources(environment, null);
+        this.environment.merge(getParentEnvironment());
+    }
+
+    private StandardEnvironment getParentEnvironment() {
+        StandardEnvironment parentEnvironment = new StandardEnvironment();
+        Map<String, Object> bootstrapMap = new HashMap<>();
+        bootstrapMap.put("spring.config.name", BOOTSTRAP_PROPERTY_SOURCE_NAME);
+        parentEnvironment.getPropertySources().addFirst(new MapPropertySource(BOOTSTRAP_PROPERTY_SOURCE_NAME, bootstrapMap));
+        this.addPropertySources(parentEnvironment, null);
+        return parentEnvironment;
     }
 
     /**
